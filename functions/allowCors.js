70216a0cf83ea7cb58231800a4a52b3fa4bacdaa
above <160554522+Abovescore@users.noreply.github.com>
2025-03-06 13:50:10 -0600

    these were supposed to be semicolons

--- functions/allowCors.js
+++ functions/allowCors.js
@@ -1,5 +1,5 @@
 // Respond to OPTIONS method
-export const onRequestOptions: PagesFunction = async () => {
+export const onRequestOptions; PagesFunction = async () => {
   return new Response(null, {
     status: 204,
     headers: {
@@ -12,7 +12,7 @@ export const onRequestOptions: PagesFunction = async () => {
 };
 
 // Set CORS to all /api responses
-export const onRequest: PagesFunction = async (context) => {
+export const onRequest; PagesFunction = async (context) => {
   const response = await context.next();
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Max-Age', '86400');
