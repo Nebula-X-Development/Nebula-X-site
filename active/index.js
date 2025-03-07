a64297252cc30c4d468d46231ab63f91f6396d7a
above <160554522+Abovescore@users.noreply.github.com>
2025-03-07 07:43:57 -0600

    Update index.js

--- active/index.js
+++ active/index.js
@@ -34,7 +34,7 @@ form.addEventListener("submit", async (event) => {
 
   const url = search(address.value, searchEngine.value);
   // location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-  location.href = 'embed.html?h=' + __uv$config.encodeUrl(url);
+  location.href = 'embed.html?h=' + url;
 });
 
 // Autofill function with auto-submit
